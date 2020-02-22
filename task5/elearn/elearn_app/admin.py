ad96c6e648dffd0805592b1dd00050dac7b1166d
Yury Kuznetsov <yurykuznetsov@protonmail.com>
2020-02-22 21:41:19 +0300

    add admin.py, few changes in models.py

--- task5/elearn/elearn_app/admin.py
+++ task5/elearn/elearn_app/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Course, Lecture, Homework, HomeworkInstance, HomeworkInstanceComment
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Lecture)
+class LectureAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Homework)
+class HomeworkAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(HomeworkInstance)
+class HomeworkInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(HomeworkInstanceComment)
+class HomeworkInstanceCommentsAdmin(admin.ModelAdmin):
+    pass
--- task5/elearn/elearn_app/models.py
+++ task5/elearn/elearn_app/models.py
@@ -5,13 +5,19 @@ from django.contrib.auth.models import User
 class Course(models.Model):
     title = models.CharField(max_length=128, null=False)
     teachers = models.ManyToManyField(User, related_name="course_teachers")
-    students = models.ManyToManyField(User, related_name="course_students")
+    students = models.ManyToManyField(User, related_name="course_students", blank=True)
+
+    def __str__(self):
+        return f"Course \"{self.title}\""
 
 
 class Lecture(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=128, null=False)
-    file = models.FileField(upload_to='lectures/', null=False)
+    file = models.FileField(upload_to="lectures/", null=False)
+
+    def __str__(self):
+        return f"Lecture \"{self.title}\""
 
 
 class Homework(models.Model):
@@ -19,21 +25,30 @@ class Homework(models.Model):
     title = models.CharField(max_length=128, null=False)
     text = models.TextField(null=False)
 
+    def __str__(self):
+        return f"Homework \"{self.title}\" from lecture \"{self.lecture.title}\""
+
 
 class HomeworkInstance(models.Model):
     class Meta:
-        unique_together = (('homework', 'student'),)
+        unique_together = (("homework", "student"),)
 
     homework = models.ForeignKey(Homework, on_delete=models.CASCADE)
     student = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    uploaded_work = models.FileField(upload_to='done_homeworks/', null=True)
+    file = models.FileField(upload_to="done_homeworks/", null=True)
     is_done = models.BooleanField(default=False)
     mark = models.SmallIntegerField(null=True)
 
+    def __str__(self):
+        return f"Homework: {self.homework.title} by {self.student}"
 
-class HomeworkInstanceComments(models.Model):
+
+class HomeworkInstanceComment(models.Model):
     homework_instance = models.ForeignKey(HomeworkInstance, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     datetime = models.DateTimeField(auto_now=True)
-    body = models.TextField(null=False)
+    body = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.author}: {self.body}"
